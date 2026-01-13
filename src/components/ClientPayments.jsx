9738e13ddb2c830e327bdf554223bd159a6a2526
thiruAji <ajithuma38@gmail.com>
2026-01-13 09:37:45 +0800

    fix: force redeploy with comment

--- src/components/ClientPayments.jsx
+++ src/components/ClientPayments.jsx
@@ -1,3 +1,4 @@
+// Client Payments Component - Fixed expense calculation
 import React, { useState, useEffect } from 'react';
 import { db } from '../firebase';
 import { collection, addDoc, onSnapshot, query, orderBy, serverTimestamp, deleteDoc, doc, updateDoc } from 'firebase/firestore';
